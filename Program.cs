0918e89bb170e36cae7aa7b5a58c6a9fcfc52dfc
geyme <geym.edik@yandex.ru>
2025-05-01 12:37:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace TheEndOfMOdule2
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Form form = new InteractiveForm("Привет, я интерактивчик");
+            Application.Run(form);
+        }
+    }
+    public class InteractiveForm : Form
+    {
+        private bool _mouseOnForm = false;
+        public InteractiveForm(string title)
+        {
+            Text = title; ;
+            Height = 600;
+            Width = 800;
+            StartPosition = FormStartPosition.CenterScreen;
+
+            Button exitButton = CreateButton(new Size(60, 30), new Point(700, 500), "Выход");
+            exitButton.Click += (sender, e) => Application.Exit();
+            Label label = CreateLabel(new Size(200, 30), new Point(300, 200), "Вы дважды щелкнули по форме");
+            label.Visible = false;
+            DoubleClick += (sender, e) => ShowLabel(label);
+            MouseEnter += (sender, e) => _mouseOnForm = true;
+            MouseLeave += (sender, e) => _mouseOnForm = false;
+        }
+
+        private void ShowLabel(Label label)
+        {
+            if (_mouseOnForm) label.Visible = true; 
+        }
+
+        private void SetcommonParameters(Control element, Size size, Point position, string title)
+        {
+            element.Size = size;
+            element.Location = position;
+            element.Text = title;
+            Controls.Add(element);
+        }
+        private Button CreateButton(Size size, Point position, string title)
+        {
+            Button button = new Button();
+            SetcommonParameters(button,size,position,title);
+            return button;
+        }
+        private Label CreateLabel(Size size, Point position, string title)
+        {
+            Label label = new Label();
+            SetcommonParameters(label, size, position, title);
+            return label;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TheEndOfMOdule2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheEndOfMOdule2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c74dcbbd-5ddd-4095-a42d-47ae090aa90e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
